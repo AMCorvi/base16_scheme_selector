99a1d1647a1828c1602e0b624314473aabf6110b
AMCorvi <am@corvi.com>
2018-11-12 19:37:00 -0500

    - Completed the main subroutine for selecting and activating base16 schemes - Completed tests for scheme.js

--- src/base16/scheme.spec.js
+++ src/base16/scheme.spec.js
@@ -5,11 +5,20 @@ describe("scheme.js", () => {
     it("should return an array of paths", done => {
       schemes()
         .then(files => {
-          console.log(files);
           expect(Array.isArray(files)).toBe(true);
           done();
         })
         .catch(console.error);
     });
+
+    it('should be a list of sh path is', function (done) {
+      schemes()
+        .then(files => {
+          const allShellFiles = files.every(file => file.match(/\.sh$/))
+          expect(allShellFiles).toBe(true);
+          done()
+        })
+        .catch(console.error)
+    })
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,15 +1,64 @@
 // import the base16 file paths
-import { listOfSchemes, arrayOfFile, readDir } from "./base16/schemes";
-import fs from 'fs'
+import fs from "fs";
+import cp from "child_process";
+import { homedir } from "os";
 
-function main() {
-  //initialize and welcome user to the scheme selector
-  // pass an array of base16 scheme paths to the selector
-  let out
-  listOfSchemes()
-    .then(console.log)
-    .catch(console.error)
-}
+import inquirer from "inquirer";
+
+import schemes from "./base16/schemes";
 
 // Run the program
-main();
+main(schemes);
+
+
+function main(schemes) {
+  schemes() // Promise which collects a list of file name of the colro schemes
+    .then(shellFiles => schemesSelector(shellFiles)) // activate interactive terminal
+    .catch(function(err) {
+      throw err; //terminal program upon failure
+    });
+}
+
+/**
+ * initiates the interactive terminal list by which the user selects
+ * which base16 color scheme they wish to use
+ * @param {array} listOfSchemes a list of file names for the various base16 color schemes
+ */
+function schemesSelector(listOfSchemes) {
+  const interogatives = [
+    {
+      type: "list", // type of menu in which to use
+      name: "scheme_selector", // name of result
+      message: "Pick a mood?", // prompt for switching styles
+      choices: listOfSchemes // selection from which to choose
+    }
+  ];
+
+  inquirer.prompt(interogatives).then(res => {
+    console.log(
+      `Activating ~/.config/base16-shell/scripts/${res.scheme_selector}`
+    );
+    return executeSchemeChange(res.scheme_selector)
+  });
+}
+
+/**
+ * spawn a new terminal process by which to execute the provided terminal scheme script
+ *
+ * @param {string} scheme filename of the desired terminal scheme script
+ * @returns {<Child_Process> } Object container of the child_process spawned to execute command
+ */
+export function executeSchemeChange(options, scheme, test_command) {
+  return cp.exec(
+    test_command || `bash ${scheme}`,
+    {
+      cwd: options.cwd || `${homedir()}/.config/base16-shell/scripts`
+    },
+    (err, stdout, stderr) => {
+      if (err)  throw err;
+      else if (stderr) return console.warn("Failure of spawn process:\n", stderr);
+      console.log(stdout);
+    }
+  );
+}
+
